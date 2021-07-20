9a8195fb9cc648a6faf2bd475d007fa490e52382
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-19 20:58:41 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text} from 'react-native';
+import styled from 'styled-components';
+import Card from './components/Card';
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Container>
+    <TitleBar>
+    <Avatar source={require('./assets/adaptive-icon.png')}/>
+      <Title>Welcome back!,</Title>
+      <Name>Sergio</Name>
+      </TitleBar>
+      <Subtitle>Continue Learning</Subtitle>
+      <Card />
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
+ }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+const Container = styled.View`
+  flex: 1;
+  background-color: lightblue;
+`;
+const Title =styled.Text`
+  font-size: 16px;
+  color: purple;
+  font-weight: 500;
+`;
+const Name=styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+const TitleBar=styled.View`
+  width: 100%;
+  margin-top:50px;
+  padding-left:20px;
+  padding-left: 80px;
+  `;
