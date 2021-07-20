47dc06f978435a76fe20d08082c11ead48ac56d9
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-20 00:14:30 -0500

    menu done, aniamtion added, data from state []

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import Card from './components/Card';
 import { Ionicons } from '@expo/vector-icons';
 import Logo from './components/Logo';
 import Course from './components/Course';
+import Menu from './components/Menu';
 
 export default class App extends React.Component {
   render(){
   return (
     <Container>
+    <Menu />
     <SafeAreaView>
     <ScrollView style={{height: "100%"}}>
     <TitleBar>
